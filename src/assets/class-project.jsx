e6a706a6401746c781e00c3b5dd416e92e6d81fa
manellylopez <manellylopezqqq@gmail.com>
2025-06-14 08:41:36 -0500

    create class for project

---
+++ src/assets/class-project.jsx
@@ -0,0 +1,12 @@
+class Project {
+    constructor(value) {
+      this.id = value;
+      this.projectName = '';
+      this.projectDescription = '';
+      this.screenshotProjectSource = '';
+      this.githubHref = '';
+      this.projectNewWindow = '';
+    }
+  }
+  
+  export { Project };
