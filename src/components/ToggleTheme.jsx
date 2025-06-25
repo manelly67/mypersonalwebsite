b4692b651a11038ad8f3c672e375414e287af379
manellylopez <manellylopezqqq@gmail.com>
2025-06-25 10:07:35 -0500

    add ToggleTheme component and behaviour

---
+++ src/components/ToggleTheme.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Icon from '@mdi/react';
+import { mdiThemeLightDark } from '@mdi/js';
+import styles from "../styles/Buttons.module.css";
+
+const ToggleTheme = (props) => {
+  const [theme, setTheme] = useState(props.theme);
+  const { buttonA } = styles;
+
+  const body = document.querySelector("body");
+  if (body) {
+    document.body.className = theme;
+  }
+
+  function toggleTheme() {
+    const newTheme = body.className === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  }
+
+  return (
+    <button
+      className={buttonA}
+      onClick={() => {
+        toggleTheme();
+      }}
+      aria-label="toggle theme dark/light"
+    >
+      <Icon path={mdiThemeLightDark} size={1} />
+    </button>
+  );
+};
+
+export default ToggleTheme;
